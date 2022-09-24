b28b5b66e5fb09f618923c83c3c55ba303f382e7
Babara Aura <baura@ev-q2.kzn.21-school.ru>
2022-09-24 16:15:21 +0300

    modified mod00/ex00

--- mod00/ex00/megaphone.cpp
+++ mod00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: baura <baura@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 12:28:41 by baura             #+#    #+#             */
-/*   Updated: 2022/09/04 12:35:34 by baura            ###   ########.fr       */
+/*   Updated: 2022/09/24 15:17:20 by baura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 int	main(int argc, char **argv)
 {
-	if (argc = 1)
+	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		
+		std::cout << "haven't finished this part yet" << std::endl;
 	}
 
 	return (0);
