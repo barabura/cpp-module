e3c0a8270a6ac89fa84047d42ab823b096f704f0
Babara Aura <baura@pr-p3.kzn.21-school.ru>
2022-10-15 20:10:50 +0300

    mod00/ex00

--- mod00/ex00/megaphone.cpp
+++ mod00/ex00/megaphone.cpp
@@ -6,20 +6,36 @@
 /*   By: baura <baura@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 12:28:41 by baura             #+#    #+#             */
-/*   Updated: 2022/09/24 15:17:20 by baura            ###   ########.fr       */
+/*   Updated: 2022/10/15 20:00:19 by baura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
 int	main(int argc, char **argv)
 {
+	int counter = 0;
+	
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		std::cout << "haven't finished this part yet" << std::endl;
+		while (++counter < argc)
+		{
+			std::string param = argv[counter];
+			for (int i = 0; i < (int)param.length(); i++)
+			{
+				std::cout << (char)(std::toupper(param[i]));
+			}
+		}	
+		std::cout << std::endl;
 	}
-
 	return (0);
 }
+
+/*
+using std::cout;
+using std::endl;
+using std::string;
+*/
